226759a03db2c20d07dfe980f9d1697cb565c4ee
yeshchyrova <e.shchyrova@gmail.com>
2024-01-15 12:15:57 +0100

    Add footer

--- src/components/ContactUs/ContactUs.tsx
+++ src/components/ContactUs/ContactUs.tsx
@@ -21,7 +21,7 @@ const ContactUs = ({ setSelectedPage }: IPage) => {
   };
   return (
     <section id="contactus" className="mx-auto w-5/6 pb-32 pt-24">
-      <motion.div onViewportEnter={() => setSelectedPage(SelectedPage.Home)}>
+      <motion.div onViewportEnter={() => setSelectedPage(SelectedPage.ContactUs)}>
         {/* HEADER */}
         <motion.div
           className="md:w-3/5"
